69bef9da7913b73e41a242026b816a4bddd21e34
Velu Prabhakar Kumaravel <48288235+vpKumaravel@users.noreply.github.com>
2024-07-30 20:46:18 +0200

    Update compareLOFvsMaxwell.py
    added execution time

--- compareLOFvsMaxwell.py
+++ compareLOFvsMaxwell.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import mne
 from mne.preprocessing import find_bad_channels_maxwell, find_bad_channels_lof
 import numpy as np
+import time
 
 # Path to the MEG data file
 file_path = 'data/subX_MEG_RAW_run1.fif'
@@ -11,7 +12,11 @@ raw = mne.io.read_raw_fif(file_path, preload=True)
 raw_copy = raw.copy()
 
 # Maxwell bad channel detection
+start_time = time.time()
 bad_channels, flat_channels, scores = find_bad_channels_maxwell(raw, return_scores=True)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time} seconds")
 print("Maxwell - Bad channels identified automatically:", bad_channels)
 print("Maxwell - Flat channels identified automatically:", flat_channels)
 
@@ -22,10 +27,13 @@ lof_threshold = 1.4 # Hyperparameter 1
 num_channels = len(picks)
 n_neighbors = int(np.sqrt(num_channels)) # Hyperparameter 2
 print(n_neighbors)
+start_time = time.time()
 # Call find_bad_channels_lof to get bad channels and scores
 bad_channels_lof, scores = find_bad_channels_lof(raw_copy, n_neighbors=n_neighbors, metric='euclidean',
                                                  threshold=lof_threshold, return_scores=True, picks=picks)
-
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time} seconds")
 # Print and mark bad channels identified by LOF
 raw_copy.info['bads'] = []
 for pick_idx, score in enumerate(scores):
@@ -33,4 +41,3 @@ for pick_idx, score in enumerate(scores):
         ch_name = raw_copy.info['chs'][picks[pick_idx]]['ch_name']
         print(f'LOF: Marking channel {ch_name} as bad with a LOF score of {score}')
         raw_copy.info['bads'].append(ch_name)
-
